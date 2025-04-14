46344562657747682e440579597a5f2386fceddb
kevinchauhan <rwnkevinchauhan@gmail.com>
2025-04-14 18:08:27 +0530

    fix: login import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-import Login from './pages/login';
+import Login from './pages/Login';
 import Header from './components/Header';
 import TabDetails from './pages/TabDetails';
 import { ToastContainer } from 'react-toastify';
